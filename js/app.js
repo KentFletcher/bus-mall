18283fb3591510a321ab961e3292dae2fb62abe8
KentFletcher <kentfletch09@gmail.com>
2020-03-17 14:32:02 -0700

    finished js functionality and adjusted css

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var allImages = [];
-// var numberOfRounds = 5;//to be st to 25 after testing!!
-// var itemsByName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum'];//add rest after testing!!
-// var votes = [];//place to store votes when product clicked on
+var clicks = 0;
+var numberOfRounds = 25;
 
 function ProductImage(name, imagePath) {
   this.name = name;
@@ -20,20 +19,20 @@ new ProductImage('bathroom', 'img/bathroom.jpg');
 new ProductImage('boots', 'img/boots.jpg');
 new ProductImage('breakfast', 'img/breakfast.jpg');
 new ProductImage('bubblegum', 'img/bubblegum.jpg');
-// new ProductImage('chair', 'img/chair.jpg');
-// new ProductImage('cthulhu', 'img/cthulhu.jpg');
-// new ProductImage('dog-duck', 'img/dog-duck.jpg');
-// new ProductImage('dragon', 'img/dragon.jpg');
-// new ProductImage('pen', 'img/pen.jpg');
-// new ProductImage('pet-sweep', 'img/pet-sweep.jpg');
-// new ProductImage('scissors', 'img/scissors.jpg');
-// new ProductImage('shark', 'img/shark.jpg');
-// new ProductImage('sweep', 'img/sweep.png');
-// new ProductImage('tauntaun', 'img/tauntaun.jpg');
-// new ProductImage('unicorn', 'img/unicorn.jpg');
-// new ProductImage('usb', 'img/usb.gif');
-// new ProductImage('water-can', 'img/water-can.jpg');
-// new ProductImage('wine-glass', 'img/wine-glass.jpg');
+new ProductImage('chair', 'img/chair.jpg');
+new ProductImage('cthulhu', 'img/cthulhu.jpg');
+new ProductImage('dog-duck', 'img/dog-duck.jpg');
+new ProductImage('dragon', 'img/dragon.jpg');
+new ProductImage('pen', 'img/pen.jpg');
+new ProductImage('pet-sweep', 'img/pet-sweep.jpg');
+new ProductImage('scissors', 'img/scissors.jpg');
+new ProductImage('shark', 'img/shark.jpg');
+new ProductImage('sweep', 'img/sweep.png');
+new ProductImage('tauntaun', 'img/tauntaun.jpg');
+new ProductImage('unicorn', 'img/unicorn.jpg');
+new ProductImage('usb', 'img/usb.gif');
+new ProductImage('water-can', 'img/water-can.jpg');
+new ProductImage('wine-glass', 'img/wine-glass.jpg');
 
 
 var image1 = document.getElementById('img1');
@@ -55,10 +54,6 @@ function genRandomImage() {
 
 //function to render 3 random images to the page.
 function renderProducts() {
-//   console.log(image1.src);
-//   console.log(image2.src);
-//   console.log(image3.src);
-
   var newImage1 = genRandomImage();
   image1.src = newImage1.imagePath;
   image1.name = newImage1.name;
@@ -76,12 +71,34 @@ function renderProducts() {
 }
 renderProducts();
 
-function clickHandler(event) {
-  console.log(event.target.name);
+//function to render list of clicked to the page
+function renderList() {
+  var listEl = document.getElementById('list');
 
+  for(var i = 0; i < allImages.length; i++) {
+    var clicked = document.createElement('li');
+    var renderInfo = ('The ' + allImages[i].name + ' was chosen ' + allImages[i].numClicked + ' times and was shown ' + allImages[i].timesRendered + ' times.');
+    clicked.textContent = renderInfo;
+    listEl.appendChild(clicked);
+  }
+}
+// renderList();
+
+function clickHandler(event) {
+  clicks++;
+  //   console.log(event.target.name);
+  var listEl = document.getElementById('list');
+  listEl.innerHTML = '';
+  renderProducts();
   for (var i = 0; i < allImages.length; i++) {
     if (allImages[i].name === event.target.name) {
       allImages[i].numClicked++;
+      console.log(clicks);
+    }if (clicks > numberOfRounds) {
+      event = false;
+      alert('Thanks, checkout your favorites!');
+      renderList();
+      break;
     }
   }
 }
@@ -89,27 +106,3 @@ function clickHandler(event) {
 image1.addEventListener('click', clickHandler);
 image2.addEventListener('click', clickHandler);
 image3.addEventListener('click', clickHandler);
-
-//function to change the picture when it is clicked on
-// function switchPicture() {
-
-// }
-
-
-
-
-
-
-//function to render list of clicked to the page
-
-// function renderList() {
-//   var listEl = document.getElementById('list');
-
-//   for(var i = 0; 1 < allImages.length; i++) {
-//     var clicked = document.createElement('li');
-//     var render = (allImages[i].name + ' was chosen ' + allImages[i].numClicked + ' times and was show ' + allImages[i].timesRendered.value + ' times.');
-//     listEl.textContent = render;
-//   }
-//   listEl.appendChild(clicked);
-// }
-// renderList();
